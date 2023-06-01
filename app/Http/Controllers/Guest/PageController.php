35d108d4653207254f699fc2d51f4aff124f527a
L-cir94 <luca.cirigliano94@hotmail.it>
2023-06-01 17:21:52 +0200

    show in page singers

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Singer;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('home');
+        $singers = (Singer::all());
+        return view('home', compact('singers'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <h1>Homepage</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($singers as $singer)
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $singer->name }}</h4>
+                            <h6 class="card-text">{{ $singer->surname }}</h6>
+                            <p class="card-text">{{ $singer->birth_date }}</p>
+                            <p class="card-text">{{ $singer->record_company }}</p>
+                            <p class="card-text">{{ $singer->genre}}</p>
+                            <p class="card-text">{{ $singer->updated_at}}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-</div>
 @endsection
